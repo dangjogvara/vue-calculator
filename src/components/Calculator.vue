2e7568a1431c2b29ba9bb35a389ddbc3f516a034
Dan Poulsen <dangjogvara@hotmail.com>
2020-08-08 23:50:16 +0100

    buttons

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="calculator">
     <div class="display">0.34454555</div>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
-    <span>Hello</span>
+    <div>AC</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>÷</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>x</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>+</div>
+    <div>0</div>
+    <div>.</div>
+    <div>=</div>
   </div>
 </template>
 
@@ -22,6 +30,7 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .calculator {
+    font-size: 40px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(50px, auto);
@@ -29,5 +38,6 @@
 
   .display {
     grid-column: 1 / 5;
+    background-color: red;
   }
 </style>
