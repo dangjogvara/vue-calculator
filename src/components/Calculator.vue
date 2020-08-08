585e6ed8708a527916f0f852f6d631a6f03e2cb5
Dan Poulsen <dangjogvara@hotmail.com>
2020-08-08 23:58:35 +0100

    styling buttons

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
     <div class="display">0.34454555</div>
-    <div>AC</div>
-    <div>+/-</div>
-    <div>%</div>
-    <div>÷</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>x</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>-</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>+</div>
-    <div>0</div>
-    <div>.</div>
-    <div>=</div>
+    <div class="btn">AC</div>
+    <div class="btn">+/-</div>
+    <div class="btn">%</div>
+    <div class="btn operator">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
   </div>
 </template>
 
@@ -39,5 +39,18 @@
   .display {
     grid-column: 1 / 5;
     background-color: red;
+  }
+
+  .zero {
+    grid-column: 1 / 3;
+  }
+
+  .btn {
+    background-color: #eee;
+    border: 1px solid;
+  }
+
+  .operator {
+    background-color: orange;
   }
 </style>
